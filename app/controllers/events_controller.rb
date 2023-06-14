9ae01e9bf701b1fe0edbdc99d803739ab1b81600
Daniel Neel <dneel10@gmail.com>
2023-06-14 00:22:03 -0400

    update creator(user) and events to allow for creation of new events by current logged in user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,31 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @events = Event.all
   end
+
+  def new
+    @event = current_user.events.new
+  end
+
+  def create
+    @event = current_user.events.create(event_params)
+
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:name, :date, :location)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  
+  resources :users
+  resources :events
+
   devise_for :users
   root to: "events#index"
 end
