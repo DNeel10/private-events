4896d2427e1a1331e1896f2236fbe9a13cf50319
Daniel Neel <dneel10@gmail.com>
2023-06-15 23:40:34 -0400

    add migration for creating through table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
 
-  has_many :attendees, class_name: "User", through: :attendances
-
+  has_many :attendees, through: :attendance, source: :event_attendee
+  has_many :attendances, foreign_key: :attended_event_id
 end
--- db/migrate/20230616025223_create_attendances.rb
+++ db/migrate/20230616025223_create_attendances.rb
@@ -1,8 +1,8 @@
 class CreateAttendances < ActiveRecord::Migration[7.0]
   def change
     create_table :attendances do |t|
-      t.belongs_to :attendee, null: false, foreign_key: true
-      t.belongs_to :event, null: false, foreign_key: true
+      t.belongs_to :event_attendee, class_name: "User", null: false, foreign_key: true
+      t.belongs_to :attended_event, class_name: "Event", null: false, foreign_key: true
 
       t.timestamps
     end
